1385109b4d1fc66cb05a7586c462ae180294083b
bahori <bahori2011ms@gmail.com>
2026-06-11 00:23:17 +0900

    first commit

---
+++ nvim/init.lua
@@ -0,0 +1,18 @@
+-- show line number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- indent
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- leader key
+vim.g.mapleader = " "
