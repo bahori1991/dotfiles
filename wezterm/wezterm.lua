d9d83d45a9ee44408f0c889ba4a25cd3d3a21966
bahori <bahori2011ms@gmail.com>
2026-06-24 22:32:59 +0900

    delete wezterm configuration file

--- wezterm/wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require("wezterm")
-
-local config = wezterm.config_builder()
-
-config.automatically_reload_config = true
-config.font = wezterm.font("HakuMoto")
-config.font_size = 11.0
-
-return config
