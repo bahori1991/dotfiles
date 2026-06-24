c8ed7a435281145f515fedc7577b6fe43a842c07
bahori <bahori2011ms@gmail.com>
2026-06-24 22:31:54 +0900

    add plugin of VS Code theme

--- nvim/lua/plugins/editor/vscode.lua
+++ nvim/lua/plugins/editor/vscode.lua
@@ -1,8 +1,19 @@
+-- ================================================================================
+-- TITLE: mofiqul/vscode.nvim
+-- ABOUT: VSCode like color scheme for Neovim
+-- LINKS: https://github.com/mofiqul/vscode.nvim
+-- ================================================================================
+
 return {
   "Mofiqul/vscode.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
+  opts = {
+    transparent = true,
+    disable_nvimtree_bg = true,
+  },
+  config = function(_, opts)
+    require("vscode").setup(opts)
     vim.cmd([[colorscheme vscode]])
   end,
 }
