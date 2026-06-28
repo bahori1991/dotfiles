dcd8a36dc9e03fbd48a7a7eb00724305491b2674
bahori <bahori2011ms@gmail.com>
2026-06-28 11:07:42 +0900

    add nvim-treesitter

--- nvim/lua/plugins/editor/nvim-treesitter.lua
+++ nvim/lua/plugins/editor/nvim-treesitter.lua
@@ -1,7 +1,60 @@
 -- ================================================================================
--- TITLE: nvim-tree/nvim-tree.lua
--- ABOUT: Manage and utilize the Tree-sitter parsing engine within Neovim
--- LINKS: https://github.com/nvim-treesitter/nvim-treesitter
+-- TITLE: neovim-treesitter/nvim-treesitter
+-- ABOUT: manage parser and query
+-- LINKS: https://github.com/neovim-treesitter/nvim-treesitter
 -- ================================================================================
 
-return {}
+return {
+  "neovim-treesitter/nvim-treesitter",
+  dependencies = { "neovim-treesitter/treesitter-parser-registry" },
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local parsers = {
+      "markdown",
+      "markdown_inline",
+      "html_tags",
+      "html",
+      "ecma",
+      "typescript",
+      "javascript",
+      "tsx",
+      "jsx",
+      "css",
+      "lua",
+      "python",
+      "yaml",
+      "toml",
+      "json",
+    }
+    local filetypes = {
+      "markdown",
+      "mdx",
+      "html",
+      "typescript",
+      "typescriptreact",
+      "javascript",
+      "css",
+      "lua",
+      "python",
+      "yaml",
+      "toml",
+      "json",
+    }
+
+    vim.api.nvim_create_user_command("TSInstallConfigured", function()
+      require("nvim-treesitter").install(parsers)
+    end, {})
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.wo.foldexpr = "v:lua.nvim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldlevel = 99
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+}
