368701ebbe08747ccbe35dad8deb6232d2fa5fdb
bahori <bahori2011ms@gmail.com>
2026-06-28 11:12:06 +0900

    add color-scheme file

---
+++ nvim/lua/config/colors.lua
@@ -0,0 +1,21 @@
+-- ================================================================================
+-- TITLE: colors.lua
+-- ABOUT: set colors of Neovim
+-- ================================================================================
+
+return {
+  bg = "#1e1e1e",
+  bg_float = "#252526",
+  bg_select = "#2a2d2e",
+  border = "#454545",
+  fg = "#d4d4d4",
+  fg_muted = "#9cdcfe",
+  red = "#f14c4c",
+  yellow = "#dcdcaa",
+  blue = "#569cd6",
+  orange = "#ce9178",
+  green = "#6a9955",
+  violet = "#c586c0",
+  cyan = "#4ec9b0",
+  gray = "#5a5a5a",
+}
