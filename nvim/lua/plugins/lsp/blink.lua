428490e1ea00aee50487bc31b27a72c1dbc36fc9
bahori <bahori2011ms@gmail.com>
2026-06-28 11:10:21 +0900

    refactor blink.cmp

--- nvim/lua/plugins/lsp/blink.lua
+++ nvim/lua/plugins/lsp/blink.lua
@@ -16,17 +16,35 @@ return {
       ["<S-Tab>"] = { "select_prev", "fallback" },
       ["<CR>"] = { "select_and_accept", "fallback" },
       ["<C-CR>"] = { "fallback" },
+      ["<Up>"] = { "fallback" },
+      ["<Down>"] = { "fallback" },
     },
     appearance = {
       nerd_font_variant = "mono",
+    },
+    signature = {
+      window = { winblend = 15 },
     },
     completion = {
       keyword = { range = "full" },
       accept = { auto_brackets = { enabled = true }, },
       list = { selection = { preselect = true, auto_insert = true } },
+      menu = { winblend = 15 },
+      documentation = {
+        window = {
+          winblend = 15
+        },
+      },
     },
     sources = {
-      default = { "lsp", "path" },
+      default = { "lazydev", "lsp", "path", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        },
+      },
     },
     fuzzy = {
       implementation = "prefer_rust_with_warning"
@@ -35,9 +53,6 @@ return {
   opts_extend = { "sources.default" },
   config = function(_, opts)
     require("blink.cmp").setup(opts)
-    vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "#1f2335" })
-    vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { bg = "#1f2335", fg = "#3b4261" })
-    vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#2b324a", fg = "NONE" })
   end,
 }
 
--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -5,45 +5,6 @@
 -- ======================================================================================
 
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "saghen/blink.cmp"
-    },
-    opts = {
-      servers = {
-        lua_ls = {},
-      }
-    },
-    config = function(_, opts)
-      for server, config in pairs(opts.servers) do
-        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-        vim.lsp.enable(server)
-      end
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    opts = {
-      automatic_enable = false
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "mason-org/mason.nvim" },
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "stylua",
-      },
-    },
-  },
+  "mason-org/mason.nvim",
+  opts = {},
 }
