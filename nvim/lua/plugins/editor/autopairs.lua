5d04dce5a8dc25832861b398dbd8577b1dec4bcb
bahori <bahori2011ms@gmail.com>
2026-06-28 11:12:27 +0900

    add autopairs plugin

---
+++ nvim/lua/plugins/editor/autopairs.lua
@@ -0,0 +1,12 @@
+-- ================================================================================
+-- TITLE: windwp/nvim-autopairs
+-- ABOUT: autopair plugin for Neovim
+-- LINKS: https://github.com/windwp/nvim-autopairs
+-- ================================================================================
+
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = true,
+  opts = {}
+}
