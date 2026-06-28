f9ecc7d726ee3b29942320ba74af05b98916ddda
bahori <bahori2011ms@gmail.com>
2026-06-28 11:13:19 +0900

    add nvim-treesitter-context plugin

---
+++ nvim/lua/plugins/editor/nvim-treesitter-context.lua
@@ -0,0 +1,13 @@
+-- ================================================================================
+-- TITLE: nvim-treesitter/nvim-treesitter-context
+-- ABOUT: enable to see the current code structre to the top
+-- LINKS: https://github.com/nvim-treesitter/nvim-treesitter-context
+-- ================================================================================
+
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  enabled = true,
+  opts = {
+    max_lines = 3
+  }
+}
