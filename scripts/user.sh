c6f9783cbd74e7796e47b40b0bbbf070d1b9a665
bahori <bahori2011ms@gmail.com>
2026-09-23 23:47:29 +0900

    refact: change the way to detect dockerenv

--- scripts/user.sh
+++ scripts/user.sh
@@ -4,16 +4,20 @@
 DOTFILES_DIR="${DOTFILES_DIR:-$HOME/.config/dotfiles}"
 
 # set a specific name if necessary
-# ${WIN_USER:=<Your Windows Username>
+# ${WIN_USER=<Your Windows Username>
 # ${WSL_User:=<Your WSL Username>
 
 # get Windows Username
-if [[ -z "$WIN_USER" || ! -d "/mnt/c/Users/$WIN_USER" ]]; then
-  _detected=$(cmd.exe /c 'echo %USERNAME%' 2>/dev/null | tr -d '\r\n')
-  if [[ -n "$_detected" && -d "/mnt/c/Users/$_detected" ]]; then
-    WIN_USER="$_detected"
+if [[ -z "${WIN_USER:-}" || ! -d "/mnt/c/Users/${WIN_USER:-}" ]]; then
+  if [ -f ./dockerenv ] || ! command -v cmd.exe >/dev/null 2>&1; then
+    : # skip Windows detection
+  else
+    _detected=$(cmd.exe /c 'echo %USERNAME%' 2>/dev/null | tr -d '\r\n')
+    if [[ -n "$_detected" && -d "/mnt/c/Users/$_detected" ]]; then
+      WIN_USER="$_detected"
+    fi
+    unset _detected
   fi
-  unset _detected
 fi
 
 export WIN_USER="${WIN_USER:-$USER}"
