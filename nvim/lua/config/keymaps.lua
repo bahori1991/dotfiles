ef254bcac46e465b83d9a43a9f039b51a12d1181
bahori <bahori2011ms@gmail.com>
2026-09-23 23:49:18 +0900

    add: nohlsearch keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,7 +3,15 @@
 -- ABOUT: set keymaps of Neovim
 -- =====================================================================================================
 
+-- move current row to center
 vim.keymap.set("n", "G", "Gzz", { noremap = true, silent = true })
-
 vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
 vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
+
+-- Redraw Neovim window
+vim.keymap.set("n", "<leader>l", "<cmd>nohlsearch<bar>redraw!<cr>", {
+  desc = "Redraw / clear search highlight",
+})
+
+-- move to end of word after uppercased
+vim.keymap.set("n", "gUiw", "gUiwe", { noremap = true })
